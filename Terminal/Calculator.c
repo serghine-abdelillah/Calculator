f90db575feea20593b087033f5a9a7ff137e02eb
Abdelillah Serghine <92404091+serghine-abdelillah@users.noreply.github.com>
2024-05-05 22:22:36 +0100

    Update Calculator.c

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -3,10 +3,10 @@
 int main()
 {
     int num1 , num2;
-    printf("Enter The frst Number :\n");
+    printf("Enter The first Number :\n");
     scanf("%d", &num1);
 
-    printf("The frst Number is %d :\n", num1);
+    printf("The first Number is %d :\n", num1);
 
     
     return 0;
