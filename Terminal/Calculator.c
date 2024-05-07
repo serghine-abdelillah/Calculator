aee96f8d76a1b37acb22c105ac667c6869785c3d
Abdelillah <abdelillah2003@gmail.com>
2024-05-07 18:28:50 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -2,14 +2,15 @@
 
 int main() {
     int num1 , num2;
-    
+    char opr;
     printf("Enter The first Number : ");
     scanf("%d", &num1);
 
     printf("Enter The Second Number : ");
     scanf("%d",&num2);
 
-    printf("The Result : %d", num1+num2);
+    printf("Enter The Operator : ");
+    scanf("%s",&opr);
 
     
     return 0;
