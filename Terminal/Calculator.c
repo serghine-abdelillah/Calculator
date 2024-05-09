a18fc88525d864b8748086a2c19b15953a8656e4
Abdelillah <abdelillah2003@gmail.com>
2024-05-09 15:47:40 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -4,22 +4,22 @@ int main() {
     float num1 , num2, result;
     char opr;
     printf("Enter The first Number : ");
-    scanf("%d", &num1);
+    scanf("%.2f", &num1);
 
     printf("Enter The Second Number : ");
-    scanf("%d",&num2);
+    scanf("%.2f",&num2);
 
     // printf("Enter One Of The Operators : + | / | * | - : ");
     // scanf("%s",&opr);
 
     result = num1 + num2;
-    printf("Result = %d\n", result);
+    printf("Result = %.2f\n", result);
 
     result = num1 - num2 ;
-    printf("Result = %d\n", result);
+    printf("Result = %.2f\n", result);
 
     result = num1 / num2;
-    printf("Result : %d", result);
+    printf("Result : %.2f", result);
 
     
     
