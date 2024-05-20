fc60863141a784e321392725f270953b1a39bf93
Abdelillah <abdelillah2003@gmail.com>
2024-05-20 17:53:35 +0100

    Enhance C Code

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -33,8 +33,8 @@ int main() {
     case '+':
         result = num1 + num2;
         break;
-    case '+':
-        result = num1 + num2;
+    case '-':
+        result = num1 - num2;
         break;
     
     default:
