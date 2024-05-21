371c9a8ebfea8ac0523e2ba04ee3a1aa3b5dea6a
Abdelillah <abdelillah2003@gmail.com>
2024-05-21 21:11:08 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -36,8 +36,8 @@ int main() {
     case '-':
         result = num1 - num2;
         break;
-    case '-':
-        result = num1 - num2;
+    case '*':
+        result = num1 * num2;
         break;
     
     default:
