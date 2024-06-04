23252fbb6107b6a635d57c768483750f47fc5971
Abdelillah <abdelillah2003@gmail.com>
2024-06-04 23:25:07 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -8,16 +8,14 @@ int main()
     char opr;
     bool x = true;
 
-    
-    scanf(" %c", &opr);
-
-    while (x == true)
+        while (x == true)
     {
         printf("Enter The first Number : ");
         scanf("%lf", &num1);
         printf("Enter The Second Number : ");
         scanf("%lf", &num2);
         printf("Enter One Of The Operators : + | / | * | - : ");
+        scanf(" %c", &opr);
         x = false;
     }
 
