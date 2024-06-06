737a2f34ef57e6b8ec0280c2432060a0bfb9b357
Abdelillah <abdelillah2003@gmail.com>
2024-06-06 19:22:04 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -26,13 +26,13 @@ int main()
             result = num1 - num2;
             printf("Result = %.2lf\n", result);
             break;
+        case '*':
+            result = num1 * num2;
+            printf("Result : %.2lf\n", result);
+            break;
             x = false;
         }
 
-    case '*':
-        result = num1 * num2;
-        printf("Result : %.2lf\n", result);
-        break;
     case '/':
         result = num1 / num2;
         printf("Result : %.2lf\n", result);
