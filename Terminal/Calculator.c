b7d63f508fe246fa40ef2a353720b7986c9abaf8
Abdelillah <abdelillah2003@gmail.com>
2024-06-09 21:49:55 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -10,13 +10,15 @@ int main()
 
     while (x == true)
     {
+
+        printf("Enter The first Number : ");
         printf("Enter The first Number : ");
         scanf("%lf", &num1);
         printf("Enter The Second Number : ");
         scanf("%lf", &num2);
         printf("Enter One Of The Operators : + | / | * | - : ");
         scanf(" %c", &opr);
-        printf("QUIT : ");
+        printf("QUIT : tap q|Q :");
         scanf(" %c", &opr);
         switch (opr)
         {
@@ -36,10 +38,10 @@ int main()
             result = num1 / num2;
             printf("Result : %.2lf\n", result);
             break;
+        case 'q' || 'Q':
+            break;
         default:
             break;
-
-            x = false;
         }
     }
     return 0;
