59a71d1f0c91322b8981fe98fae15c92ba16d640
Abdelillah <abdelillah2003@gmail.com>
2024-06-10 23:37:26 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -12,12 +12,15 @@ int main()
     {
 
         printf("Enter The first Number : ");
+        
+        printf("Enter One Of The Operators : + | / | * | - : ");
+        scanf(" %c", &opr);
+
+
         printf("Enter The first Number : ");
         scanf("%lf", &num1);
         printf("Enter The Second Number : ");
         scanf("%lf", &num2);
-        printf("Enter One Of The Operators : + | / | * | - : ");
-        scanf(" %c", &opr);
         printf("QUIT : tap q|Q :");
         scanf(" %c", &opr);
         switch (opr)
