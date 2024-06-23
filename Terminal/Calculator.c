3fa62750ebc5b6618805098247a192a13fdc0cb3
Abdelillah <abdelillah2003@gmail.com>
2024-06-23 21:16:04 +0100

    Update C File

--- Terminal/Calculator.c
+++ Terminal/Calculator.c
@@ -18,55 +18,56 @@ int main()
         printf("Division / : D/d ");
         printf("QUIT : tap q|Q :");
         scanf(" %c", &choice);
-        while (choice != "Q" || choice !="q"){
-        printf("Enter The first Number : ");
-        scanf("%lf", &num1);
-        printf("Enter The Second Number : ");
-        scanf("%lf", &num2);
-        if (choice == "A" || choice == "a")
+        while (choice != "Q" || choice != "q")
         {
-            result = num1 + num2;
-            printf("Result = %.2lf\n", result);
-            break;
+            printf("Enter The first Number : ");
+            scanf("%lf", &num1);
+            printf("Enter The Second Number : ");
+            scanf("%lf", &num2);
+            if (choice == "A" || choice == "a")
+            {
+                result = num1 + num2;
+                printf("Result = %.2lf\n", result);
+                break;
+            }
+            else if (choice == "S" || choice == "s")
+            {
+                result = num1 - num2;
+                printf("Result = %.2lf\n", result);
+                break;
+            }
+            else if (choice == "M" || choice == "d")
+            {
+                result = num1 * num2;
+                printf("Result : %.2lf\n", result);
+                break;
+            }
+            else if (choice == "D" || choice == "d")
+            {
+                result = num1 / num2;
+                printf("Result : %.2lf\n", result);
+                break;
+            }
+            else if (choice == "Q" || choice == "q")
+            {
+                x = false;
+            }
+            else
+            {
+                printf("Invalid Choice\n");
+            }
         }
-        else if (choice == "S" || choice == "s")
-        {
-            /* code */
-        }
-        else if (choice == "C" || choice == "c")
-        {
-            /* code */
-        }
-        else if (choice == "D" || choice == "d")
-        {
-            /* code */
-        }
-        else if (choice == "Q" || choice == "q")
-        {
-            x = false;
-        }
-        else
-        {
-            printf("Invalid Choice\n");
-        }
-        }
-        
+
         switch (opr)
         {
         case '+':
-            
+
         case '-':
-            result = num1 - num2;
-            printf("Result = %.2lf\n", result);
-            break;
+
         case '*':
-            result = num1 * num2;
-            printf("Result : %.2lf\n", result);
-            break;
+
         case '/':
-            result = num1 / num2;
-            printf("Result : %.2lf\n", result);
-            break;
+
         case 'q' || 'Q':
             break;
         default:
