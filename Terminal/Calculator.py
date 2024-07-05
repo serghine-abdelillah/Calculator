f993ae3f38a27c2526a7091a86ab171f3b048d0b
Abdelillah <abdelillah2003@gmail.com>
2024-07-05 19:18:01 +0100

    Python Intialized Fucntions

--- Terminal/Calculator.py
+++ Terminal/Calculator.py
@@ -1,1 +1,15 @@
 # develop a simple calculator
+
+# Function to add two numbers
+def add (x,y):
+    return x+y
+
+def soustraction(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
