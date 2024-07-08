511e1b58179e1d08319dd369861bf98c72effaad
Abdelillah <abdelillah2003@gmail.com>
2024-07-08 17:36:16 +0100

    Update Python File

--- Terminal/Calculator.py
+++ Terminal/Calculator.py
@@ -34,3 +34,6 @@ elif choice == 3:
     num2 = float(input("Enter second number: "))
     print(num1,"*",num2,"=", multiply(num1,num2))
 elif choice == 4:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(num1,"/",num2,"=", divide(num1,num2))
