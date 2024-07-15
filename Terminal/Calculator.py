9de74feab892dc9a65d5d9bf5727bcef9a5918ab
Abdelillah <abdelillah2003@gmail.com>
2024-07-15 14:28:18 +0100

    Add Loop to The Menu

--- Terminal/Calculator.py
+++ Terminal/Calculator.py
@@ -12,32 +12,36 @@ def multiply(x,y):
 
 def divide(x,y):
     return x/y
+x = True
+while x == True :
+    print("--- MENU ---")
+    print("Select operation :")
+    print("1.Add : "  )
+    print("2.Substract : ")
+    print("3.Multiply : ")
+    print("4.Divide : ")
+    print("Q|q.Quit : ")
 
-print("--- MENU ---")
-print("Select operation :")
-print("1.Add : "  )
-print("2.Substract : ")
-print("3.Multiply : ")
-print("4.Divide : ")
-print("Q|q.Quit : ")
 
 
-
-# Take input from the user
-choice = input("Enter choice(1/2/3/4):")
-if choice == "1":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print(num1,"+",num2,"=", add(num1,num2))
-elif choice == "2":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print(num1,"-",num2,"=", soustraction(num1,num2))
-elif choice == "3":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print(num1,"*",num2,"=", multiply(num1,num2))
-elif choice == "4":
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print(num1,"/",num2,"=", divide(num1,num2))
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4/Q|q):")
+    if choice == "1":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(num1,"+",num2,"=", add(num1,num2))
+    elif choice == "2":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(num1,"-",num2,"=", soustraction(num1,num2))
+    elif choice == "3":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(num1,"*",num2,"=", multiply(num1,num2))
+    elif choice == "4":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print(num1,"/",num2,"=", divide(num1,num2))
+    elif choice == "Q" or "q":
+        print("Quitting")
+        x = False
