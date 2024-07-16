25f33da82c621d1398b1317ffda5fdf8972e4f58
Abdelillah <abdelillah2003@gmail.com>
2024-07-16 18:49:16 +0100

    Java File Started

---
+++ Terminal/Calculator.java
@@ -0,0 +1,11 @@
+/* develop calculator */
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
