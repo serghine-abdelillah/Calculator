26129bc9620207bb2682acd3cd1739336e509521
Abdelillah <abdelillah2003@gmail.com>
2024-07-18 16:44:31 +0100

    Update Java File

--- Terminal/Calculator.java
+++ Terminal/Calculator.java
@@ -1,18 +1,24 @@
 /* develop calculator */
 public class Calculator {
-    public int  add (int a, int b) {
+    public float  add (float a, float b) {
         return a + b;
     }
-    public int  soustraction (int a, int b) {
+    public float  soustraction (float a, float b) {
+        return a - b;
+    }
+    public float  multiply (float a, float b) {
+        return a - b;
+    }
+    public float  soustraction (float a, float b) {
         return a - b;
     }
 
     public static void main(String[] args) {
-        int a = 4;
-        int b = 2;
-        System.out.println("Addition: " + (a + b));
-        System.out.println("Subtraction: " + (a - b));
-        System.out.println("Multiplication: " + (a * b));
-        System.out.println("Division: " + (a / b));
+        float a = 4;
+        float b = 2;
+        System.out.prfloatln("Addition: " + (a + b));
+        System.out.prfloatln("Subtraction: " + (a - b));
+        System.out.prfloatln("Multiplication: " + (a * b));
+        System.out.prfloatln("Division: " + (a / b));
     }
 }
