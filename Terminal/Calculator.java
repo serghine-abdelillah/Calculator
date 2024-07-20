b27bd6b3f2455df1ee87d002444ccf011c9062c8
Abdelillah <abdelillah2003@gmail.com>
2024-07-20 12:20:56 +0100

    Add Menu to Java File

--- Terminal/Calculator.java
+++ Terminal/Calculator.java
@@ -14,6 +14,42 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
+
+        /* add menu */
+        while (true) {
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice: ");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            if (choice == 5) {
+                break;
+            }
+            System.out.println("Enter two numbers: ");
+            float a = sc.nextFloat();
+            float b = sc.nextFloat();
+            Calculator cal = new Calculator();
+            switch (choice) {
+                case 1:
+                    System.out.println("Addition: " + cal.add(a, b));
+                    break;
+                case 2:
+                    System.out.println("Subtraction: " + cal.soustraction(a, b));
+                    break;
+                case 3:
+                    System.out.println("Multiplication: " + cal.multiply(a, b));
+                    break;
+                case 4:
+                    System.out.println("Division: " + cal.divide(a, b));
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+            
+        }
         float a = 4;
         float b = 2;
         System.out.println("Addition: " + (a + b));
