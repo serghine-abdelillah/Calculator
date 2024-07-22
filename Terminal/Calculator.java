b2b1f3c8b51a2c0b9d740231fb8046f79c21f08a
Abdelillah <abdelillah2003@gmail.com>
2024-07-22 10:58:51 +0100

    Java File DONE

--- Terminal/Calculator.java
+++ Terminal/Calculator.java
@@ -20,37 +20,37 @@ public class Calculator {
 
         /* add menu */
         while (true) {
+            System.out.println("MENU");
             System.out.println("1. Addition");
             System.out.println("2. Subtraction");
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
             System.out.println("5. Exit");
             System.out.println("Enter your choice: ");
-            try (Scanner sc = new Scanner(System.in)) {
-                int choice = sc.nextInt();
-                if (choice == 5) {
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            if (choice == 5) {
+                break;
+            }
+            System.out.println("Enter two numbers: ");
+            float a = sc.nextFloat();
+            float b = sc.nextFloat();
+            Calculator cal = new Calculator();
+            switch (choice) {
+                case 1:
+                    System.out.println("Addition: " + cal.add(a, b));
                     break;
-                }
-                System.out.println("Enter two numbers: ");
-                float a = sc.nextFloat();
-                float b = sc.nextFloat();
-                Calculator cal = new Calculator();
-                switch (choice) {
-                    case 1:
-                        System.out.println("Addition: " + cal.add(a, b));
-                        break;
-                    case 2:
-                        System.out.println("Subtraction: " + cal.soustraction(a, b));
-                        break;
-                    case 3:
-                        System.out.println("Multiplication: " + cal.multiply(a, b));
-                        break;
-                    case 4:
-                        System.out.println("Division: " + cal.divide(a, b));
-                        break;
-                    default:
-                        System.out.println("Invalid choice");
-                }
+                case 2:
+                    System.out.println("Subtraction: " + cal.soustraction(a, b));
+                    break;
+                case 3:
+                    System.out.println("Multiplication: " + cal.multiply(a, b));
+                    break;
+                case 4:
+                    System.out.println("Division: " + cal.divide(a, b));
+                    break;
+                default:
+                    System.out.println("Invalid choice");
             }
             
         }
