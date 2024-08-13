07092417676f0d061e06cb3f6d976f8a577066a7
Abdelillah <abdelillah2003@gmail.com>
2024-08-13 23:34:24 +0100

    Update JS File

--- Terminal/calculator.js
+++ Terminal/calculator.js
@@ -11,22 +11,25 @@ function Multiply(a, b) {
 function Divide(a, b) {
   return a / b;
 }
-while (true) {
-  var choice = prompt("Enter your choice: ");
-  if (choice == `1` || choice == `2` || choice == `3` || choice == `4`) {
-    console.log();
-    var num1,
-      num2 = prompt("Enter two numbers");
-    break;
-  }
-  console.log("Invalid choice. Please enter a valid choice.");
-}
 
 console.log("MENU");
 console.log("1. Add");
 console.log("2. Subtract");
 console.log("3. Multiply");
 console.log("4. Divide");
+
+
+while (true) {
+  var choice = prompt("Enter your choice: ");
+  if (choice == `1` || choice == `2` || choice == `3` || choice == `4`) {
+    console.log();
+    var num1,num2 = prompt("Enter two numbers");
+    break;
+  }
+  console.log("Invalid choice. Please enter a valid choice.");
+}
+
+
 
 if (choice == `1`) {
   console.log(add(1, 2));
