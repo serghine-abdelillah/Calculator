d8b3f4270906616c7dc01b2932b1742ce1d85a4c
Abdelillah <abdelillah2003@gmail.com>
2024-09-18 20:36:27 +0100

    fixing errors

--- Terminal/calculator.js
+++ Terminal/calculator.js
@@ -13,18 +13,19 @@ function Divide(a, b) {
   return a / b;
 }
 
-console.log("MENU");
-console.log("1. Add");
-console.log("2. Subtract");
-console.log("3. Multiply");
-console.log("4. Divide");
-console.log("5. Quit");
 
-var choice = prompt("Enter your choice: ");
-while (choice != 5) {
-  
+var x = true 
+while (x) {
+  console.log("MENU");
+  console.log("1. Add");
+  console.log("2. Subtract");
+  console.log("3. Multiply");
+  console.log("4. Divide");
+  console.log("5. Quit");
+  var choice = prompt("Enter your choice: ");
   if (choice == `1` || choice == `2` || choice == `3` || choice == `4`) {
-    var choice = prompt("You can change the choice, Enter your choice: ");
+    
+    
     console.log("Enter two numbers : ");
     const num1 = prompt("Enter the first number : ");
     const num2 = prompt("Enter the second numbers : ");
@@ -44,6 +45,7 @@ while (choice != 5) {
     
     } else if (choice == `5`) {
       console.log(`Quitting`);
+
     break;
   } 
 }else {
