edb8f7193f36e640e8106a9f595f23e5dbd273b1
Abdelillah <abdelillah2003@gmail.com>
2024-12-24 12:40:20 +0100

    JS, HTML, CSS : Updated Files

---
+++ App/webApp/main.js
@@ -0,0 +1,7 @@
+// when click on div diplat message 
+// when click on button display message
+
+document.querySelector('div').addEventListener('click', function() {
+    alert('Div is clicked');
+});
+
