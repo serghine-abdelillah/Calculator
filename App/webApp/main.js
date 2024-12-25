90701ea71afad2ac14b93a4f9c95e0244c09a1dd
Abdelillah <abdelillah2003@gmail.com>
2024-12-25 22:47:21 +0100

    JS : Updated File

--- App/webApp/main.js
+++ App/webApp/main.js
@@ -4,7 +4,7 @@
 document.querySelectorAll('button').forEach(function(button) {
     button.addEventListener('click', function() {
         // parse html element
-        var message = button.outerHTML;
+        var message = button.innerText;
         alert(message);
     });
 });
