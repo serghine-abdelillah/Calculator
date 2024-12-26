6f14ac7761f7fa08422f00dc8a9e292944797ee1
Abdelillah <abdelillah2003@gmail.com>
2024-12-26 22:40:40 +0100

    JS, CSS : Updated File

--- App/webApp/main.js
+++ App/webApp/main.js
@@ -3,7 +3,18 @@
 
 document.querySelectorAll('.numbers-buttons button').forEach(function(button) {
     button.addEventListener('click', function() {
-        // parse html element
+        // append number to the span
         document.querySelector('span').appendChild(document.createTextNode(button.innerText));
+    });
+});
+
+document.querySelectorAll('.operations-buttons button').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // append operation to the span
+        const span = document.querySelector('span');
+        const lastChar = span.innerText.slice(-1);
+        if (!['+', '-', '/', 'x'].includes(lastChar)) {
+            span.appendChild(document.createTextNode(button.innerText));
+        }
     });
 });
