e8e7b6ec1f1cb5598ae9b1d8adb72922f2990441
Abdelillah <abdelillah2003@gmail.com>
2024-12-27 22:49:41 +0100

    JS : Updated File

--- App/webApp/main.js
+++ App/webApp/main.js
@@ -1,20 +1,9 @@
 // when click on div diplat message 
 // when click on button display message
 
-document.querySelectorAll('.numbers-buttons button').forEach(function(button) {
-    button.addEventListener('click', function() {
-        // append number to the span
-        document.querySelector('span').appendChild(document.createTextNode(button.innerText));
-    });
-});
-
-document.querySelectorAll('.operations-buttons button').forEach(function(button) {
-    button.addEventListener('click', function() {
-        // append operation to the span
-        const span = document.querySelector('span');
-        const lastChar = span.innerText.slice(-1);
-        if (!['+', '-', '/', 'x'].includes(lastChar)) {
-            span.appendChild(document.createTextNode(button.innerText));
-        }
-    });
-});
+document.querySelectorAll('button').forEach(function (button) {
+    button.addEventListener('click', function(){
+        screen = document.querySelector('span');
+        txt = document.createTextNode(button.innerText)
+        screen.appendChild(txt)});    
+})
