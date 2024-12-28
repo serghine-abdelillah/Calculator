9de73867324153f5fed63811fdd8356e6c64aaaf
Abdelillah <abdelillah2003@gmail.com>
2024-12-28 23:01:21 +0100

    JS : Updated File

--- App/webApp/main.js
+++ App/webApp/main.js
@@ -1,10 +1,16 @@
 // when click on div diplat message 
 // when click on button display message
 var screen = document.querySelector('span');
+var curnum = ''
+var prenum = ''
+var opr = ''
 var result = 0;
 
 document.querySelector('.ac').addEventListener('click', function(){
     screen.innerText = '';
+    curnum = ''
+    prenum = ''
+    opr = ''
 })
 document.querySelectorAll('.numbers-buttons button ').forEach(function (button) {
     button.addEventListener('click', function(){
